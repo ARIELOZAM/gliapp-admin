0cc41a788cee5ee4aad898cdd468113c9edd07af
Lizeth Vazquez <lizethvazquez@MacBook-Pro-de-Lizeth.local>
2021-09-20 23:14:04 -0500

    Nuxt install

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="content">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Hello</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
